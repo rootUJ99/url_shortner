0d5ee078ffec13e94143bd445d93babcf8092280
ujwal arak <ujwal99arak@gmail.com>
2023-09-06 00:46:15 +0530

    added basic gorilla mux route

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/rootuj99/url_shortner
 
 go 1.20
 
-require (
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pressly/goose v2.7.0+incompatible // indirect
-)
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 func main(){
 	fmt.Println("Hello form go")
+	r:= mux.NewRouter()	
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("this is gorilla\n"))
+	})
+	http.ListenAndServe(":6969",r)
 }
